6423e184c0d247d719a8085079ca785a7e707b0d
Martin Kaldma <martin.kaldma@exsitec.se>
2022-08-16 20:17:33 +0200

    Adding print logging..

--- scripts/battery/dalybms_connector.py
+++ scripts/battery/dalybms_connector.py
@@ -17,8 +17,11 @@ bms_mac = "96:69:08:01:05:08"
 logging.basicConfig(filename='dalybms_connector_log.log', level=logging.DEBUG)
 
 async def read_bms():
+    print("read_bms:")
     bms = DalyBMSBluetooth()
+    print("connecting to " + bms_mac)
     await bms.connect(bms_mac)
+    print("connected, getting data... ")
     all_data = await bms.get_all()
     #mosfet_data = await bms.set_discharge_mosfet(True)
     print(all_data)
